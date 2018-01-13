9f9022bc7bfd37e6351dc4d79041978d41b09c9e
Melovin <pedidamelovin@gmail.com>
2018-01-13 17:51:53 -0600

    update

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -1,10 +1,14 @@
-create procedure pr_menu @user int
+alter procedure pr_menu @user int
 as
 begin
+	-- pr_menu 1
 	select	d.module_group_name,
 			c.module_name,
 			c.link,
-			d.icon as group_icon
+			d.icon as group_icon,
+			c.id as module_id,
+			d.id as group_id,
+			c.seq
 	from	sys_user a
 	
 			inner join sys_user_access b on
@@ -16,4 +20,7 @@ begin
 			inner join sys_module_group d on
 			d.id = c.module_group_id
 	where	a.id = @user
+			and c.show = 1
+	order by isnull(c.seq,1000)
 end
+
